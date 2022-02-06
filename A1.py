e7d3aa10483bd90169094b2055d3a0d4b0bfd20e
zakaisma <zakaisma@zakaisma-m-xgty.hitronhub.home>
2022-02-05 22:45:20 -0500

    Start of assignment. Hopefully this goes well haha...

---
+++ A1.py
@@ -0,0 +1,18 @@
+from lib2to3.pgen2.token import ISTERMINAL
+
+
+#!/usr/bin/env python3
+####################################################
+# COMP3106 - Introduction to Artificial Intelligence
+# Assignment 1
+
+# February 2022, Zakaria Ismail
+
+# Copyright (c) 2022 by Cisco Systems, Inc. 
+# All rights reserved.
+####################################################
+
+
+
+if __name__ == "__main__":
+    print("Hello world.")
